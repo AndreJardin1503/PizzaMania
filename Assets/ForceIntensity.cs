103c88525b58e4464730b5aaca14d4e845b99efb
AndreJardin1503 <booguie2004@gmail.com>
2026-01-09 14:52:19 +0000

    Cube Throw Properties Updated

--- Assets/ForceIntensity.cs
+++ Assets/ForceIntensity.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ForceIntensity : MonoBehaviour
@@ -18,8 +20,38 @@ public class ForceIntensity : MonoBehaviour
     }
 
     public void applyForce () {
-        this.GetComponent<Rigidbody>().AddForce(Vector3.up*intensidade,ForceMode.Impulse);
 
+        //StartCoroutine(MyCoroutine());
+
+        this.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+
+        this.gameObject.GetComponent<Rigidbody>().AddForce(this.transform.forward*intensidade,ForceMode.Impulse);
+            print(this.GetComponent<Rigidbody>().linearVelocity);
+            print(this.transform.forward);
+            print(intensidade);
+
+
+    }
+
+     IEnumerator MyCoroutine()
+    {
+        // Loop indefinitely (or until stopped)
+        
+        
+            Debug.Log("Coroutine started: " + Time.time);
+
+            // Wait for 1 second (scaled time)
+            yield return new WaitForSeconds(0.2f);
+
+            this.gameObject.GetComponent<Rigidbody>().AddForce(this.transform.forward*intensidade + this.transform.up*intensidade,ForceMode.Impulse);
+            print(this.GetComponent<Rigidbody>().linearVelocity);
+            print(this.transform.forward);
+            print(intensidade);
+
+            Debug.Log("Coroutine resumed after 1 second: " + Time.time);
+
+            // Perform your action here, e.g., instantiate a new object
+            // GameObject newObject = Instantiate(myPrefab, transform.position, Quaternion.identity);
     }
 
 
