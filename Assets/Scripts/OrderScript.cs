94fe7006606d68abf2b1742da14f849823e4f09c
José Bernardo Ferreira <97bernardoferreira2004@gmail.com>
2026-01-22 01:07:15 +0000

    last changes (again)

--- Assets/Scripts/OrderScript.cs
+++ Assets/Scripts/OrderScript.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OrderScript : MonoBehaviour
 {
@@ -12,6 +13,15 @@ public class OrderScript : MonoBehaviour
     private GameObject pineimg;
     private GameObject mushimg;
     private GameObject pepperimg;
+
+
+    private float startTime = 180f;
+    private TMP_Text timerText;
+    private GameObject reset;
+    private float currentTime;
+    private bool timerRunning;
+    private float resetTimer = 0;
+    private bool isReseting;
 
     private GameObject[] images;
 
@@ -31,6 +41,12 @@ public class OrderScript : MonoBehaviour
 
     private void Start()
     {
+        reset = GameObject.FindWithTag("ResetText").transform.Find("Reseter").gameObject;
+        timerText = GameObject.FindWithTag("Timer").GetComponent<TMP_Text>();
+        currentTime = startTime;
+        timerRunning = true;
+        UpdateTimerText();
+
         fishimg = GameObject.FindWithTag("ImageFish");
         pepeimg = GameObject.FindWithTag("ImagePepe");
         oliveimg = GameObject.FindWithTag("ImageOlive");
@@ -87,6 +103,66 @@ public class OrderScript : MonoBehaviour
         images[currentIngredient3].transform.Find("Cross").gameObject.SetActive(false);
 
 
+    }
+
+    private void Update()
+    {
+        if (isReseting)
+        {
+            resetTimer += Time.deltaTime;
+            Debug.Log(resetTimer);
+            if (resetTimer > 3)
+            {
+                SceneManager.LoadScene("BernasScene");
+            }
+        }
+
+        if (!timerRunning) return;
+
+        currentTime -= Time.deltaTime;
+
+        if (currentTime <= 0f)
+        {
+            currentTime = 0f;
+            timerRunning = false;
+            UpdateTimerText();
+            OnTimerFinished();
+        }
+        else
+        {
+            UpdateTimerText();
+        }
+
+
+    
+    }
+
+
+    private void UpdateTimerText()
+    {
+        int minutes = Mathf.FloorToInt(currentTime / 60f);
+        int seconds = Mathf.FloorToInt(currentTime % 60f);
+
+        timerText.text = $"{minutes:00}:{seconds:00}";
+
+        if (currentTime <= 10f)
+        {
+            timerText.color = Color.red;
+        }
+    }
+    private void OnTimerFinished()
+    {
+        reset.SetActive(true);
+        isReseting = true;
+
+    }
+
+    public void RestartTimer(float newTime)
+    {
+        startTime = newTime;
+        currentTime = startTime;
+        timerRunning = true;
+        UpdateTimerText();
     }
 
     public void UpdateBoard(bool[] hasIngredients)
